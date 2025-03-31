5fb33e97d1be1ffbcbf655a4220e13cf011c5eee
Jdyjjjiikhfgik55845 <Jdyjjjiikhfgik55845@gmail.com>
2025-03-31 21:32:00 +0530

    Update queue.py

--- ISTKHAR_MUSIC/utils/inline/queue.py
+++ ISTKHAR_MUSIC/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🫧 𝗢𝘄𝗻𝗲𝗿 🫧", url=f"http://t.me/RAM_HACKER_RP"
+                text="🫧 𝗢𝘄𝗻𝗲𝗿 🫧", url=f"http://t.me/Jani_Owner"
             ),
             InlineKeyboardButton(
-                text="🍬 𝗦𝘂𝗽𝗽𝗼𝗿𝘁🍬", url=f"https://t.me/Dream143292"
+                text="🍬 𝗦𝘂𝗽𝗽𝗼𝗿𝘁🍬", url=f"https://t.me/Jani_Govind_Chat_Group"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
